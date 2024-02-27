64ea5078913674046277e45a08f7f10baacad7ac
JD <jangu2464@gmail.com>
2024-02-27 21:19:02 +0100

    Inicialización proyecto GIT

---
+++ src/main/java/com/mycompany/miproyectonb/MiproyectoNB.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.miproyectonb;
+
+/**
+ *
+ * @author Vespertino
+ */
+public class MiproyectoNB {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
